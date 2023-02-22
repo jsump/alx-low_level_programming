b6dc117c85ece6e854ae97dce185eac3287db1fd
jsump <libbyn79@gmail.com>
2023-02-22 10:00:09 -0800

    Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+#include<stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+inr convert_day(int month, int day);
+#endif
+
