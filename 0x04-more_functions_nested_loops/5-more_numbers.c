c99b582fdabc749ceed8db6e2531b2ca8fe73679
jsump <libbyn79@gmail.com>
2023-02-23 05:13:12 -0800

    Prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers  prints 10 times the numbers, from 0 to 14
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by new line
  * Return: Always 0
  */
@@ -14,7 +14,7 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
