f260d4ead4fe8d2e94810721aa82066e80fa364f
jsump <libbyn79@gmail.com>
2023-02-27 05:23:35 -0800

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,12 @@ void rev_string(char *s)
 	int counter = 0;
 	int i;
 
-	while (s[conter] != '\0')
+	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
-		rev = s[i[;
+		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
 	}
