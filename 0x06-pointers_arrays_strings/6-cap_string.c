c9cd8759f6523a99dc7c2d2e1277428e72be3949
jsump <libbyn79@gmail.com>
2023-03-02 07:38:08 -0800

    Capitalizes all words(first letter) of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,21 +14,21 @@ char *cap_string(char *str)
 		i++;
 
 		if (str[i - 1] == ' ' ||
-				str[i] == '\t' ||
-				str[i] == '\n' ||
-				str[i] == ',' ||
-				str[i] == ';' ||
-				str[i] == '.' ||
-				str[i] == '!' ||
-				str[i] == '?' ||
-				str[i] == '"' ||
-				str[i] == ')' ||
-				str[i] == '(' ||
-				str[i] == ')' ||
-				str[i] == '{' ||
-				str[i] == '}' ||
-				i == 0)
-			str[i] -= 32;
+			str[i] == '\t' ||
+			str[i] == '\n' ||
+			str[i] == ',' ||
+			str[i] == ';' ||
+			str[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == ')' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}' ||
+			i == 0)
+		str[i] -= 32;
 		i++;
 	}
 	return (str);
