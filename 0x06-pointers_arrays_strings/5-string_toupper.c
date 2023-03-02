1a60c7863fdd50bbe27183d71e6de393be0eecde
jsump <libbyn79@gmail.com>
2023-03-02 07:10:51 -0800

    Lowecase to uppercase in string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @n: pointer
+ * Return: 0
+ */
+char *string_toupper(char *n)
+{
+	int p;
+
+	p = 0;
+	while (n[p] != '\0')
+	{
+		if (n[p] >= 'a' && n[p] <= 'z')
+			n[p] = n[p] - 32;
+		p++;
+	}
+	return (n);
+}
