bb0cf8ae9f04ef55cc08d053e00999a4084f5edc
jsump <libbyn79@gmail.com>
2023-03-20 07:39:48 -0700

    Write a fuction that freesa dog/frees memory allocated to struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dog by freeing memoey allocated for struct dog
+ * @d: struct dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
