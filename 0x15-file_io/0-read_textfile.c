d9b56555458bd08fbfe6aa58b7c30ca253b91ee4
jsump <libbyn79@gmail.com>
2023-04-09 13:35:17 -0700

    Function that reads and prints a textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,6 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	FILE *filepointer;
 	char *buffer;
 	ssize_t readbytes;
+	ssize_t writtenbytes;
 
 	buffer = malloc(letters + 1);
 
@@ -41,6 +42,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	buffer[readbytes] = '\0';
 
+	writtenbytes = write(STDOUT_FILENO, buffer, readbytes);
+	
+	if (writtenbytes == -1 || writtenbytes != readbytes)
+	{
+		fclose(filepointer);
+		free(buffer);
+		return (0);
+	}
 	printf("%s", buffer);
 
 	fclose(filepointer);
