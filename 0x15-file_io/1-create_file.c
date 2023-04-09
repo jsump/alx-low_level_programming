d5227e25293aa6231d75e8993912f10f94aa51aa
jsump <libbyn79@gmail.com>
2023-04-09 15:08:11 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * create_file - creates a file
  * @filename: name of file
@@ -7,4 +8,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	FILE *filepointer;
+	size_t byteswritten;
+	size_t length;
+	int retvl = 1;
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	filepointer = fopen(filename, "w");
+	if (filepointer == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+		byteswritten = fwrite(text_content, sizeof(char), length, filepointer);
+		if (byteswritten != length)
+		{
+			retvl = -1;
+		}
+	}
+	fclose(filepointer);
+	return (return_value);
+}
