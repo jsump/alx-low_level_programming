52ebfff2dcb75ebad70bbbea131199d335759cf6
jsump <libbyn79@gmail.com>
2023-04-10 07:35:52 -0700

    Copy content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,7 @@
  * @file_from: file to be copied
  * @file_to: file that has been copied
  * Return: exit status
+ *
  */
 void file_copy(char *file_from, char *file_to)
 {
@@ -19,19 +20,14 @@ void file_copy(char *file_from, char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR, "Error: Can't read from file %s. \n", file_from);
-		exit(98);
-	}
-	if (readbytes == -1)
-	{
-		dprintf(STDERR, "Error: Can't read from file %s. \n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s. \n", file_from);
 		exit(98);
 	}
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR, "Error: Can't write to %s. \n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s. \n", file_to);
 		exit(99);
 	}
 	while ((readbytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -39,26 +35,26 @@ void file_copy(char *file_from, char *file_to)
 		writtenbytes = write(fd_to, buffer, readbytes);
 		if (writtenbytes == -1)
 		{
-			dprintf(STDERR, "Error: Can't write to %s. \n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s. \n", file_to);
 			exit(99);
 		}
 	}
 
-	close(fd_from, file_from);
-	close(fd_to, file_to);
-}
 
-/**
- * file_close - closes a file
- * @fd: file descriptor
- * @filename: name of file
- * Return: exit status
- */
-void file_close(int fd, char *filename)
-{
-	if (close(fd) == -1)
+	if (readbytes == -1)
 	{
-		dprintf(stderr, "Error: Cant't close fd %d, \n", fd);
+		close(fd_from);
+		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s. \n", file_from);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't close fd %d, \n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d, \n", fd_to);
 		exit(100);
 	}
 }
@@ -72,7 +68,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_copy(argv[1], argv[2]);
