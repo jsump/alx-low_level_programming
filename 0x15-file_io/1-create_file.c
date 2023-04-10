180ef161fb0bf172bfd2aeedff34c8303efd0f33
jsump <libbyn79@gmail.com>
2023-04-10 08:35:45 -0700

    Create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		byteswritten = fwrite(text_content, sizeof(char), length, filepointer);
 		if (byteswritten != length)
 		{
-			return (1);
+			return (-1);
 		}
 	}
 	fclose(filepointer);
