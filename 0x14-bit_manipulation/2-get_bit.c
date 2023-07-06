d32101f4246ef2647cec1eba1d77fdb17bba87f3
jsump <libbyn79@gmail.com>
2023-07-06 08:00:20 -0700

    Return bitval at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get-bit - returns value of bit at given index
+ * @index: index starting from 0 to get bit you want
+ * Return: value of bit at index @index or -1 if an erroe occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int position_of_bit = (n >> index) & 1;
+	
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	return (position_of_bit);
+}
