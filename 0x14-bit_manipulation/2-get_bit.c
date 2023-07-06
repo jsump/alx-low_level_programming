c4cb7403addd14c1ee84b3e1c13f40935a5ac6b4
jsump <libbyn79@gmail.com>
2023-07-06 08:09:53 -0700

    Return bitval at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int position_of_bit = (n >> index) & 1;
 
-	return (position_of_bit);	
-
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-
+	return (position_of_bit);
 }
