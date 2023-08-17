90d88f5ef3675883abc53a4adfe1e8fa654ebdcd
jsump <libbyn79@gmail.com>
2023-08-17 05:38:24 -0700

    Print all elements of dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *curr = h;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (curr != NULL)
 	{
 		printf("%d\n", curr->n);
 		curr = curr->next;
 		count++;
 	}
 	return (count);
+}
