762ea17cc86fd7fced8c2fe948cbc377da01bc95
jsump <libbyn79@gmail.com>
2023-09-07 10:11:32 -0700

    Print hash table properly

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,20 +9,31 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int long i;
 	hash_node_t *current;
+	int comma_flag;
+
+	comma_flag = 0;
 
 	if (ht == NULL)
 		return;
 
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		current = ht->array[i];
 
 		while (current != NULL)
 		{
-			printf("{%s: %s, }", current->key, current->value);
+			if (comma_flag == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", current->key, current->value);
+
+			comma_flag = 1;
 			current = current->next;
 		}
 	}
+	printf("}\n");
+
 }
 
 
