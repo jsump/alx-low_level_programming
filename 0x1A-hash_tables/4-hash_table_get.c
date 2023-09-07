9058243295b8d8b8b6037d68f0a998ea550b6fce
jsump <libbyn79@gmail.com>
2023-09-07 10:53:39 -0700

    Retrieve value associated with key properly

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,11 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
-	index = key_index((const unsigned char *)key, ht->size);
-
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
 	while (current != NULL)
